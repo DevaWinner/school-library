b74c9b20711705e16f1d809aaced1893dccb3d15
DevaWinner <winneraaniekan@gmail.com>
2023-09-09 00:51:48 +0100

    Add attribute to Student class

--- students.rb
+++ students.rb
@@ -1,10 +1,13 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+    super(name, age, parent_permission: parent_permission)
     @classroom = classroom
   end
+
 
   def play_hooky
     '¯(ツ)/¯'
