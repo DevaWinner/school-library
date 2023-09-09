490a0743ae631467fc706e28a1a5d53465f2385f
DevaWinner <winneraaniekan@gmail.com>
2023-09-09 08:49:29 +0100

    Create App class and initialize

---
+++ app.rb
@@ -0,0 +1,15 @@
+require_relative 'person'
+require_relative 'book'
+require_relative 'rental'
+require_relative 'teacher'
+require_relative 'students'
+
+class App
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+  end
+
+  
+end
